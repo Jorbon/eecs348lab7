cc29accb164eb53351f7c7e7cbfe5fca401ad134
Jorbon <ben.a.phillips@outlook.com>
2024-04-04 21:53:27 -0500

    content commit

---
+++ practice4.php
@@ -0,0 +1,19 @@
+<?php
+
+$size = $_POST["size"];
+
+echo "<table border=1><tr><td></td>";
+for ($i = 1; $i <= $size; $i++) {
+	echo "<td>$i</td>";
+}
+echo "</tr>";
+
+for ($i = 1; $i < $size; $i++) {
+	echo "<tr><td>$i</td>";
+    for ($j = 1; $j <= $size; $j++) {
+        echo "<td>", $i * $j, "</td>";
+    }
+    echo "</tr>";
+}
+
+?>
