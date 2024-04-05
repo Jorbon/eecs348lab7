b4f4bcb955c62df214fbecf1d0e135096b79f204
Jorbon <ben.a.phillips@outlook.com>
2024-04-04 22:10:11 -0500

    php bug

--- practice4.php
+++ practice4.php
@@ -8,7 +8,7 @@ for ($i = 1; $i <= $size; $i++) {
 }
 echo "</tr>";
 
-for ($i = 1; $i < $size; $i++) {
+for ($i = 1; $i <= $size; $i++) {
 	echo "<tr><td>$i</td>";
     for ($j = 1; $j <= $size; $j++) {
         echo "<td>", $i * $j, "</td>";
